1399e0b17cf447168452d4ecdcd88851a07383da
DysphoricUnicorn <taxcamp.de@gmail.com>
2020-02-05 21:37:14 +0100

    Wrote a wrapper around twig

---
+++ Controllers/baseController.php
@@ -0,0 +1,26 @@
+<?php
+
+	namespace Controllers;
+
+	use Twig\Error\LoaderError;
+	use Twig\Error\RuntimeError;
+	use Twig\Error\SyntaxError;
+
+	class baseController {
+
+		/**
+		 * Renders a view and returns it as a string.
+		 * @param string $name <p>The name of the template relative to /Views without the .twig.html ending</p>
+		 * @param array $props <p>Any variables you want to give to the view</p>
+		 * @return string
+		 * @throws LoaderError
+		 * @throws RuntimeError
+		 * @throws SyntaxError
+		 */
+		final function renderView (string $name, array $props) {
+			$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../Views');
+			$twig = new \Twig\Environment($loader, ['cache' => __DIR__ . '/../Storage/ViewCache']);
+			return $twig->render($name . '.twig.html', $props);
+		}
+
+	}
