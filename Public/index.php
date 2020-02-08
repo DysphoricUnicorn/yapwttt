c49be120503236cd4fe805eb4e0693353892ded4
DysphoricUnicorn <taxcamp.de@gmail.com>
2020-02-08 17:56:36 +0100

    Improved theme functionality

--- Public/index.php
+++ Public/index.php
@@ -5,15 +5,7 @@
 
 	use Controllers\loginController;
 
-	if (isset($_GET['theme'])) {
-		switch ($_GET['theme']) {
-			default:
-				define("theme", "solarized");
-				break;
-		}
-	} else {
-		define("theme", "solarized");
-	}
+	setTheme();
 
 	$route = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_URL);
 
@@ -22,4 +14,34 @@
 			$lc = new loginController();
 			echo($lc->showLoginView());
 			break;
+	}
+
+	function setTheme () {
+		if (isset($_GET['theme'])) {
+			$theme = $_GET['theme'];
+		} else if (isset($_SESSION['theme'])) {
+			$theme = $_SESSION['theme'];
+		} else if (isset($_COOKIE['theme'])) {
+			$theme = $_SESSION['theme'];
+		}
+		if (isset($theme)) {
+			switch ($theme) {
+				default:
+					define("theme", "solarized");
+					break;
+			}
+		} else {
+			define("theme", "solarized");
+		}
+		$_SESSION['theme'] = constant('theme');
+		setcookie('theme', constant('theme'));
+	}
+
+	/**
+	 * Starts a session if none is active
+	 */
+	function prepareSession () {
+		if (session_status() === PHP_SESSION_NONE) {
+			session_start();
+		}
 	}
